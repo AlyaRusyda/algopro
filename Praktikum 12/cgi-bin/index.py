f9143000aee95b4d4ca03f50dfa524fc2321f72f
alyarusydam@gmail.com <alyarusydam@gmail.com>
2024-12-31 06:55:39 +0700

    update foto

--- Praktikum 12/cgi-bin/index.py
+++ Praktikum 12/cgi-bin/index.py
@@ -1,12 +1,17 @@
-print("<!DOCTYPE html>\n")
-print("""<html>
+#!/usr/bin/env python3
+
+print("Content-Type: text/html\n")
+print("<!DOCTYPE html>")
+print("""
+<html>
 <head><title>Akses dengan CGI</title></head>
-<body>""")
-print(""""
-      <div style="float: left; margin-right: 20px;>
-        <img src="foto.png" alt="foto alya" width="170" height="230">
+<body>
+""")
+print("""
+      <div style="float: left; margin-right: 20px;">
+        <img src="/foto.png" alt="foto alya" width="170" height="230">
       </div>
-      """)
+""")
 print("""
     <h3>Data Diri</h3>
     <p>Nama: Alya Rusyda Maharistya</p>
@@ -14,4 +19,5 @@ print("""
     <p>Tempat, tanggal lahir: Sukoharjo, 31 Juli 2006</p>
     <p>Tempat wisata favorit: Pantai</p>
     <p>Motto: Mantap</p>
-      """)
+""")
+print("</body></html>")
